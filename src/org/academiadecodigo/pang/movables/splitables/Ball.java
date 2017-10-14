34c074568982726508be32833c469cd861297da6
Gustavo Novais <gnsilva92@gmail.com>
2017-10-14 12:08:44 +0100

    Implemented moveLeft moveRight and check boundaries

--- src/org/academiadecodigo/pang/movables/splitables/Ball.java
+++ src/org/academiadecodigo/pang/movables/splitables/Ball.java
@@ -13,10 +13,58 @@ public class Ball implements Splittable {
     private Game g;
     private Direction direction;
 
+
+    public Ball() {
+        pos = new Position( int x, int y, int width, int height);
+        direction = Direction.RIGHT;
+    }
+
+
     @Override
     public void move() {
+        switch (direction) {
 
+            case UP:
+                moveUp();
+                break;
+            case RIGHT:
+                moveRight();
+                break;
+            case DOWN:
+                moveDown();
+                break;
+            case LEFT:
+                moveLeft();
+                break;
+        }
     }
+
+        private void moveUp(){
+
+        }
+
+        private void moveRight(){
+            pos.setX(pos.getX() + 1);
+        }
+
+        private void moveDown(){
+
+        }
+
+        private void moveLeft(){
+            pos.setX(pos.getX() - 1);
+        }
+
+        private void checkBoundaries(){
+            if(direction == Direction.RIGHT && pos.getX() + pos.getWidth() >= g.getWidth()){
+                direction = direction.getOpposite();
+            }
+            if (direction == Direction.LEFT && pos.getX() <= 0){
+                direction = direction.getOpposite();
+            }
+
+        }
+
 
     @Override
     public Splittable[] split() {
