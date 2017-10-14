31657f5c1498f45ff806ac9e54426f315a699edb
Francisco Machado <franm912@gmail.com>
2017-10-14 11:18:43 +0100

    Game constructors

--- src/org/academiadecodigo/pang/Game.java
+++ src/org/academiadecodigo/pang/Game.java
@@ -3,6 +3,7 @@ package org.academiadecodigo.pang;
 import org.academiadecodigo.pang.keyboardListener.KeyboardListener;
 import org.academiadecodigo.pang.movables.Player;
 import org.academiadecodigo.pang.movables.splitables.Splittable;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 import java.util.LinkedList;
 
@@ -15,8 +16,24 @@ public class Game {
     private LinkedList<Splittable> balls;
     private KeyboardListener kb;
 
-    private final int width = 900;
-    private final int height = 500;
+    private Rectangle background;
+    private final int PADDING = 10;
+
+    private int width = 900;
+    private int height = 500;
+
+    public Game() {
+        background = new Rectangle(PADDING, PADDING, width, height);
+        background.fill();
+    }
+
+    public Game(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        background = new Rectangle(PADDING, PADDING, this.width, this.height);
+        background.fill();
+    }
 
     public void init() {
 
