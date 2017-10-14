8ddaeda3702b733a7c51ab847ab505430d4563f9
Francisco Machado <franm912@gmail.com>
2017-10-14 11:02:45 +0100

    Direction getOpposite

--- src/org/academiadecodigo/pang/directions/Direction.java
+++ src/org/academiadecodigo/pang/directions/Direction.java
@@ -7,5 +7,26 @@ public enum Direction {
     UP,
     RIGHT,
     DOWN,
-    LEFT
+    LEFT;
+
+    public Direction getOpposite() {
+
+        switch (this) {
+
+            case UP:
+                return DOWN;
+
+            case RIGHT:
+                return LEFT;
+
+            case DOWN:
+                return UP;
+
+            case LEFT:
+                return RIGHT;
+
+            default:
+                return UP;
+        }
+    }
 }
