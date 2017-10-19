9a60bd6102d6ad766e2b5962e91767098e70e3af
Francisco Machado <franm912@gmail.com>
2017-10-19 10:36:58 +0100

    Refactor factory

--- src/org/academiadecodigo/pang/movables/splitables/SplittableFactory.java
+++ src/org/academiadecodigo/pang/movables/splitables/SplittableFactory.java
@@ -13,44 +13,34 @@ public abstract class SplittableFactory {
     public static LinkedList<Splittable> getSplittableList(Game g, int level) {
 
         LinkedList<Splittable> splittables = new LinkedList<>();
-        Splittable[] objects;
         Ball ball;
 
         switch (level) {
             case 0:
                 ball = new Ball(g, 40, 40, 100, Direction.RIGHT);
-                objects = new Splittable[]{ball};
+                splittables.add(ball);
                 break;
 
             case 1:
-                objects = new Splittable[2];
-
                 ball = new Ball(g, 40, 40, 100, Direction.RIGHT);
-                objects[0] = ball;
+                splittables.add(ball);
 
                 ball = new Ball(g, g.getWidth() - 40 - 100, 40, 100, Direction.LEFT);
-                objects[1] = ball;
+                splittables.add(ball);
                 break;
 
             default:
             case 2:
-                objects = new Splittable[3];
-
                 ball = new Ball(g, 40, 40, 100, Direction.RIGHT);
-                objects[0] = ball;
+                splittables.add(ball);
 
                 ball = new Ball(g, g.getWidth() - 40 - 100, 40, 100, Direction.LEFT);
-                objects[1] = ball;
+                splittables.add(ball);
 
-                ball = new Ball(g, g.getWidth() / 2 - 50, 40, 100, null);
-                objects[2] = ball;
+                ball = new Ball(g, g.getWidth() / 2 - 50, 120, 100, null);
+                splittables.add(ball);
                 break;
         }
-
-        for (Splittable s : objects) {
-            splittables.add(s);
-        }
-
 
         return splittables;
     }
